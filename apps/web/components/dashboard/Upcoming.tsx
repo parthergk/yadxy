a299740f615f495fd070abe9929f2036a869f082
parthergk <gauravkumar81464@gmail.com>
2025-12-29 21:19:25 +0530

    fix the over due card

--- apps/web/components/dashboard/Upcoming.tsx
+++ apps/web/components/dashboard/Upcoming.tsx
@@ -18,8 +18,8 @@ const Upcoming:React.FC<Props> = ({upcomingDues}) => {
   return (
     <div className="flex flex-col px-4 py-2 shadow-lg shadow-black/10 border border-white/50 rounded-xl min-h-[287px]">
       {/* header */}
-      <h2 className=" border-b pb-2 text-xl text-heading">Upcoming Due Dates</h2>
-      <div className=" my-5 flex flex-col gap-2">
+      <h2 className=" border-b pb-2 text-xl text-heading">Over Due Record</h2>
+      <div className=" my-5 flex flex-col gap-2 h-full max-h-56 overflow-y-auto">
         {upcomingDues.length > 0 ? upcomingDues.map((item) => (
           <div key={item.name} className=" flex justify-between items-center">
             <div className="text-[13px] md:text-sm lg:text-base leading-snug text-heading">
@@ -30,7 +30,7 @@ const Upcoming:React.FC<Props> = ({upcomingDues}) => {
             </div>
             <div className=" border rounded-2xl px-2 border-primary text-primary">{item.daysOverdue} days overdue</div>
           </div>
-        )): <div className=" text-center text-sub text-lg">No Upcoming Due Record</div>}
+        )): <div className=" text-center text-sub text-lg">No Over Due Record</div>}
       </div>
     </div>
   );
