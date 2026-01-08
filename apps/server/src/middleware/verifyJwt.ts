1014f32166e96b92ea674459f7e69544a23eca3a
parthergk <gauravkumar81464@gmail.com>
2026-01-08 20:37:43 +0530

    verify with payload

--- apps/server/src/middleware/verifyJwt.ts
+++ apps/server/src/middleware/verifyJwt.ts
@@ -1,18 +1,29 @@
 import { NextFunction, Request, Response } from "express";
 import { getToken } from "next-auth/jwt";
+import { jwtDecrypt } from "jose";
 
-export async function verifyJwt(req: Request, res: Response, next: NextFunction):Promise<void> {
+export async function verifyJwt(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
   try {
-console.log(req.headers["x-forwarded-proto"]);
-    
+    const raw = req.headers.cookie;
+    const match = raw.match(/__Secure-next-auth\.session-token=([^;]+)/);
+
+    const secret = new TextEncoder().encode(process.env.NEXTAUTH_SECRET);
+
+    const { payload } = await jwtDecrypt(match[1], secret);
+    console.log("DECRYPTED PAYLOAD", payload);
+
     const token = await getToken({
       req: { cookies: req.cookies, headers: req.headers } as any,
       secret: process.env.NEXTAUTH_SECRET,
     });
-    
+
     if (!token) {
-       res.status(401).json({success: false, error: "Not authenticated" });
-       return;
+      res.status(401).json({ success: false, error: "Not authenticated" });
+      return;
     }
 
     req.user = {
@@ -24,6 +35,6 @@ console.log(req.headers["x-forwarded-proto"]);
     next();
   } catch (err) {
     console.error("ERROR: ", err);
-    res.status(401).json({success: false, error: "Invalid token" });
+    res.status(401).json({ success: false, error: "Invalid token" });
   }
 }
